822a18f766cb63baabc316131807e21bc5c4d4f4
Troy Chryssos <github email address>
2016-08-01 18:03:06 -0400

    Updated triangle class to allow self determination of type and created a triangle error message Added a kind method to the class Triangle which determines what kind of triangle it is being called on by checking for unique side lengths, and which raises an error message if any of the sides is less than or equal to 0 or if any two sides added up would be less than the third side. This was facilitated by a super smart sorting which assigned a local variable to the longest side, allowing for more succinct testing.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :side_a, :side_b, :side_c
+  
+  def initialize(side_a, side_b, side_c)
+    @side_a=side_a
+    @side_b=side_b
+    @side_c=side_c
+  end
+
+  def kind
+    test_a, test_b, test_c = [@side_a, @side_b, @side_c].sort
+    raise TriangleError if test_a<=0 || test_a+test_b<=test_c
+    case [@side_a, @side_b, @side_c].uniq.size
+      when 1 
+        :equilateral
+      when 2
+        :isosceles
+      else
+        :scalene
+    end
+  end
+end
+
+class TriangleError<StandardError
+  "Hey bro, gotta check the lengths of your sides. Somethin' ain't right."
 end
